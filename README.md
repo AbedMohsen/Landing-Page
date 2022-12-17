b40944bdf3542d33991de5a70bb13a99fe9e5988
Abed Mohsen <114285253+AbedMohsen@users.noreply.github.com>
2022-12-17 17:32:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Landing-Page
+# Landing Page Project
+
+## Table of Contents
+
+- [Landing Page Project](#landing-page-project)
+  - [Table of Contents](#table-of-contents)
+  - [Instructions](#instructions)
+  - [Development](#development)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+## Development
+
+I have added a fourth section in HTML and a scroll behaviour in CSS. I have created a navigation function that creates a new list to the upper bar whenever a new section is added in the HTML and navigates through the desired section upon selection. Also, a function that detects if a certain section is in view or not for the background animation.
---
+++ js/app.js
@@ -0,0 +1,86 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// Build the navigation bar
+
+function navigation(){
+    sections.forEach(function (section) { // Loop over sections
+        const createList = document.createElement('li');  // Create li elements in the navbar
+        createList.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;  // Add HTML to list
+        document.querySelector('#navbar__list').appendChild(createList);  // Add li elements to the ul parent
+        });
+    document.querySelector('.navbar__menu').appendChild(document.querySelector('#navbar__list'));  // Add the ul to the nav bar
+}
+
+navigation();  // Invoke the function
+
+
+// Add class 'active' to section when near top of viewport
+
+function active(){
+    const activeSection = document.querySelectorAll(".menu__link");  // Select all "menu__link" classes
+    sections.forEach(function (section, i) {  // Loop over sections
+        const viewport = section.getBoundingClientRect();  // Provide information about the size of an element and its position relative to the viewport
+        if (viewport.top <= 680 && viewport.bottom >= 300) {  // Check if the section is in viewport or not according to top and bottom
+            section.classList.add("your-active-class");  // Add 'your-active-class' if it's in view/focus
+        } else {
+            section.classList.remove("your-active-class");  // Remove 'your-active-class' if it's not in view/focus
+        }
+    })
+}
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+document.addEventListener('scroll', function() {
+    active();
+});  // Listen for a scroll to invoke
